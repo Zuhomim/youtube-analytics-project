8630dabd9f6a2df3506d0d1b297e3b5b14733469
Dmitri Sementsov <pluribus.zuhi@gmail.com>
2023-10-23 10:16:24 +0700

    fixed Playlist: del classmethods

--- src/playlist.py
+++ src/playlist.py
@@ -1,7 +1,6 @@
 import isodate
 from datetime import timedelta
 from src.apimixin import APIMixin
-import json
 
 
 class PlayList(APIMixin):
@@ -13,33 +12,30 @@ class PlayList(APIMixin):
         self.url = f'https://www.youtube.com/playlist?list={playlist_id}'
         self.__total_duration = self.get_total_duration()
 
-    @classmethod
-    def get_playlist_json(cls, playlist_id):
+    def get_playlist_json(self, playlist_id):
         """Возвращает словарь плейлиста по его id (playlist_id)"""
 
-        playlist_json = json.dumps(cls.get_service().playlists().list(id=playlist_id,
-                                                                      part='snippet,contentDetails',
-                                                                      maxResults=50,
-                                                                      ).execute(), indent=2)
+        playlist_json = self.get_service().playlists().list(id=playlist_id,
+                                                            part='snippet,contentDetails',
+                                                            maxResults=50,
+                                                            ).execute()
         return playlist_json
 
-    @classmethod
-    def get_playlist(cls, playlist_id):
-        playlist = json.loads(cls.get_playlist_json(playlist_id))["items"][0]
+    def get_playlist(self, playlist_id):
+        playlist = self.get_playlist_json(playlist_id)["items"][0]
         return playlist
 
-    @classmethod
-    def get_videos_by_playlist_id(cls, playlist_id) -> dict:
+    def get_videos_by_playlist_id(self, playlist_id) -> dict:
         """Возвращает словарь со всеми видео данного плейлиста"""
 
-        playlist_videos = cls.get_service().playlistItems().list(playlistId=playlist_id,
-                                                                 part='contentDetails',
-                                                                 maxResults=50,
-                                                                 ).execute()
+        playlist_videos = self.get_service().playlistItems().list(playlistId=playlist_id,
+                                                                  part='contentDetails',
+                                                                  maxResults=50,
+                                                                  ).execute()
         video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
-        return cls.get_service().videos().list(part='contentDetails,statistics',
-                                               id=','.join(video_ids)
-                                               ).execute()
+        return self.get_service().videos().list(part='contentDetails,statistics',
+                                                id=','.join(video_ids)
+                                                ).execute()
 
     def show_best_video(self) -> str:
         """Возвращает video-dict с максимальным likeCount"""
